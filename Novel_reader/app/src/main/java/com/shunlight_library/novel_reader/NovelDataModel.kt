e62a5ce7382d24f72bb0ff1b7273d46cd34d0003
eightman <eightman124@gmail.com>
2025-05-02 18:20:22 +0900

    UP

--- Novel_reader/app/src/main/java/com/shunlight_library/novel_reader/NovelDataModel.kt
+++ Novel_reader/app/src/main/java/com/shunlight_library/novel_reader/NovelDataModel.kt
@@ -3,6 +3,9 @@ package com.shunlight_library.novel_reader
 data class Novel(
     val title: String,
     val ncode: String,
+    val author: String,
+    val mainTag: String,
+    val subTag: String,
     val lastReadEpisode: Int = 1,
     val totalEpisodes: Int = 0,
     val unreadCount: Int = 0
